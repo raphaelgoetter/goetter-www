eb8ce1ee4b2d72d67e49cdc9b54cd42b8d7bd1e8
Raphaël Goetter <raphael@goetter.fr>
2023-11-11 22:45:17 +0100

    fix: renomme 404.html en error.html

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        error: resolve(__dirname, '404.html')
+        error: resolve(__dirname, 'error.html')
       },
     },
   },
