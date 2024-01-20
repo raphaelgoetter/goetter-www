79a017549420bb939510873ec9c374384b8768eb
Raphaël Goetter <raphael@goetter.fr>
2024-01-20 16:33:17 +0100

    feat(config): configure UnoCSS

--- uno.config.ts
+++ uno.config.ts
@@ -2,4 +2,11 @@ import { defineConfig } from 'unocss'
 
 export default defineConfig({
   presets: [],
-})
+  theme: {
+    colors: {
+      black: '#000000',
+      white: '#FFFFFF',
+      hotpink: '#FF69B4',
+    },
+  },
+}) as unknown
