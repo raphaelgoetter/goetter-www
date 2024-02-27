facf358164a61fbd7b1870a95b106214ec006e34
Raphaël Goetter <raphael@goetter.fr>
2024-02-27 21:26:17 +0100

    config: ajoute breakpoints

--- uno.config.ts
+++ uno.config.ts
@@ -25,6 +25,13 @@ export default defineConfig({
      * Configuration des valeurs du projet, utilisables en custom properties
      * ou classes utilitaires
      */
+    breakpoints: {
+      sm: "40rem", // 640px
+      md: "48rem", // 768px
+      lg: "64rem", // 1024px
+      xl: "80rem", // 1280px
+      "2xl": "96rem", // 1536px
+    },
     colors: {
       black: "#000000",
       white: "#FFFFFF",
