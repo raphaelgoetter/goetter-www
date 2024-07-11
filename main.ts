f4b392b82ad63a8f51216e3d0fd588a2f6711190
Raphaël Goetter <raphael@goetter.fr>
2024-07-11 10:56:12 +0200

    Dark mode switcher

--- main.ts
+++ main.ts
@@ -1,2 +1,39 @@
-import './styles.css'
-import 'virtual:uno.css'
+import "./styles.css"
+import "virtual:uno.css"
+
+// Light Dark mode switcher
+// JS Source : https://hidde.blog/dark-light/
+var button = document.querySelector(".theme-switcher")
+var prefersDark = window.matchMedia("(prefers-color-scheme: dark)")
+var currentTheme
+
+if (localStorage.getItem("theme-preference")) {
+  currentTheme = localStorage.getItem("theme-preference")
+} else if (prefersDark.matches) {
+  currentTheme = "dark"
+} else {
+  // default
+  currentTheme = "light"
+}
+
+button.addEventListener("click", function (event) {
+  currentTheme =
+    document.documentElement.getAttribute("data-theme-preference") === "dark"
+      ? "light"
+      : "dark"
+  setTheme(currentTheme)
+})
+
+prefersDark.addEventListener("change", function (event) {
+  currentTheme = event.matches ? "dark" : "light"
+  setTheme(currentTheme)
+})
+
+function setTheme(currentTheme) {
+  var pressed = currentTheme === "dark" ? "true" : "false"
+  document.documentElement.setAttribute("data-theme-preference", currentTheme)
+  localStorage.setItem("theme-preference", currentTheme)
+  button.setAttribute("aria-pressed", pressed)
+}
+
+setTheme(currentTheme)
