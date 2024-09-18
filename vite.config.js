2791d058e9c12c6dbc591644947bae44414a297a
Raphaël Goetter <raphael@goetter.fr>
2024-09-18 07:29:24 +0200

    fix(404): modifie le fichier `error` en `404` pour GH Pages

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        error: resolve(__dirname, "error.html"),
+        error: resolve(__dirname, "404.html"),
       },
     },
   },
