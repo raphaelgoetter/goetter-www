c4480a3a4bdea0fd499626879201611584a7e906
Raphaël Goetter <raphael@goetter.fr>
2025-01-22 16:05:11 +0100

    style(js): ajout de propriétés isHome et isError pour la gestion des pages

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,12 @@ import handlebars from "vite-plugin-handlebars"
 // https://www.npmjs.com/package/vite-plugin-handlebars
 const pageData = {
   "/index.html": {
+    isHome: true,
     title: "Raphaël Goetter, CSS addict",
   },
   "/error.html": {
-    title: "Erreur 404",
+    isError: true,
+    title: "Erreur 404, fichier non trouvé",
   },
 }
 
@@ -17,7 +19,9 @@ const pageData = {
 export default defineConfig({
   plugins: [
     handlebars({
-      data: pageData,
+      context(pagePath) {
+        return pageData[pagePath]
+      },
       partialDirectory: resolve(__dirname, "assets/partials"),
     }),
   ],
