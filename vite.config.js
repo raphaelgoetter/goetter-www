32b11a998f00f9acfcea433e81460994fbae2ae0
Raphael <raphael@goetter.fr>
2025-05-27 09:21:56 +0200

    feat: ajoute le plugin Vite Image Optimizer

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import { resolve } from "path"
 import { defineConfig } from "vite"
 import handlebars from "vite-plugin-handlebars"
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer"
 
 // https://www.npmjs.com/package/vite-plugin-handlebars
 const pageData = {
@@ -23,6 +24,24 @@ export default defineConfig({
         return pageData[pagePath]
       },
       partialDirectory: resolve(__dirname, "assets/partials"),
+    }),
+    ViteImageOptimizer({
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 90,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 90,
+      },
+      webp: {
+        // https://sharp.pixelplumbing.com/api-output#webp
+        lossless: true,
+      },
+      avif: {
+        // https://sharp.pixelplumbing.com/api-output#avif
+        lossless: true,
+      },
     }),
   ],
 })
